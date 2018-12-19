243b095d82032a228511df3ff950deabcb9593c6
ktej95 <kteja1989@msitprogram.net>
2018-12-19 09:48:12 +0530

    M8

--- ASSIGNMENT_1/M8A1.py
+++ ASSIGNMENT_1/M8A1.py
@@ -37,7 +37,26 @@ def main():
 
     scket = socket.socket()
     scket.bind((host, port))
+import socket
 
+def main():
+    host = "127.0.0.1"
+    port = 3128
+
+    scket = socket.socket()
+    scket.connect((host, port))
+
+    message = input("->")
+    while message != 'q':
+        scket.send(message.encode())
+        data = scket.recv(1024).decode()
+        print("Received from server " + data)
+        message = input("->")
+    socket.close()
+
+
+if __name__ == '__main__':
+main()
     scket.listen(1)
     conn, addr = scket.accept()
     print("Connection from " + str(addr))
